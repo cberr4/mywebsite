8fa0959b3e8b1f444aa66551ebe71f6a91e1eb6a
Charles Peace <charlespeace@hotmail.com>
2022-11-02 21:40:08 -0700

    import csstransition and prepare to restructure react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Particle from './components/Particle'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Page from './components/Page'
 import FadeIn from 'react-fade-in'
+import { CSSTransition } from 'react-transition-group'
 
 function App() {
   // For Navigation
@@ -14,19 +15,25 @@ function App() {
   ])
   const [currentPage, SetCurrent] = useState(pages[0])
 
+  const [inProp] = useState(false);
+  const nodeRef = useRef(null);
+
   return (
     <>
       <Particle/>
-      <FadeIn transitionDuration={2500}>
-        <div className='text-slate-200 font-mono'>
-          <Header>
-            <Nav pages={pages} SetCurrent={SetCurrent} currentPage={currentPage}></Nav>
-          </Header>
-          <main>
-            <Page currentPage={currentPage}></Page>
-          </main>
-        </div>
-      </FadeIn>
+      <CSSTransition nodeRef={nodeRef} in={inProp} timeout={1000} classNames="my-node">
+        <div ref={nodeRef}/>
+      </CSSTransition>
+        <FadeIn transitionDuration={2500}>
+          <div className='text-slate-200 font-mono'>
+            <Header>
+              <Nav pages={pages} SetCurrent={SetCurrent} currentPage={currentPage}></Nav>
+            </Header>
+            <main>
+              <Page currentPage={currentPage}></Page>
+            </main>
+          </div>
+        </FadeIn>
     </>
   );
 }
