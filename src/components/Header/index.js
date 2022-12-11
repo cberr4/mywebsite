841439042362a918e32c751edb7243ad8c7ba8be
Charles Peace <charlespeace@hotmail.com>
2022-12-10 21:03:00 -0800

    adjust styling for header on mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header(location) {
     return (
-        <header className='flex pt-8 md:pt-0'>
+        <header className='flex'>
             <h1 className='font-bold flex-auto px-8 py-8 lg:text-lg xl:text-2xl'>Charles Peace</h1>
             {location.children}
         </header>
