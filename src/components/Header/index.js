70e984ece36af493c03cb3573b8bbd069e4a112c
Charles Peace <charlespeace@hotmail.com>
2022-12-13 00:51:26 -0800

    adjust nav spacing and style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(location) {
     return (
         <header className='flex'>
-            <h1 className='italic flex-auto pl-8 pr-3 py-8 lg:text-lg xl:text-2xl'>Charles Peace</h1>
+            <h1 className='italic font-bold flex-auto pl-8 pr-3 py-8 lg:text-lg xl:text-2xl'>Charles Peace</h1>
             {location.children}
         </header>
     )
